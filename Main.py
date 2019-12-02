39c6e4d07c7b6aa0e74b8d0d7d26d2696881274f
leif-behrens <info@leifbehrens.de>
2019-12-02 21:03:06 +0100

    Die entsprechenden Computerinformationswerte ausgelesen und dem value-labels geschrieben; Den Wert für die Länge der Value-labels in eine Variable geschrieben und auf 400 gesetzt

--- Main.py
+++ Main.py
@@ -2,6 +2,8 @@ from PyQt5 import QtGui
 from PyQt5.QtWidgets import QApplication, QMainWindow, QTabWidget, QWidget, QPushButton, QLabel
 from PyQt5.QtCore import Qt, QRect
 import sys
+from functions import *
+import psutil
 
 
 class Monitoring(QMainWindow):
@@ -16,15 +18,16 @@ class Monitoring(QMainWindow):
         self.initWindow()
 
     def initWindow(self):
+        # Mainwindow Einstellungen
         self.setWindowTitle(self.title)
         self.setWindowIcon(QtGui.QIcon(self.icon))
         self.setFixedSize(self.width, self.height)
 
-        
+        # Tabwidget erstellt
         self.tabWidget = QTabWidget(self)
         self.tabWidget.setGeometry(QRect(0, 0, 800, 550))
         
-        # Initiate all tabs
+        # Tabs werden initialisiert
         self.initMonitoring()    
         self.initComputerinformation()
         self.initLogs()        
@@ -32,7 +35,7 @@ class Monitoring(QMainWindow):
         self.initLoadFile()
         self.initGraph()
         
-        
+        # Anzeige der GUI
         self.show()
 
 
@@ -65,6 +68,19 @@ class Monitoring(QMainWindow):
         self.btn_disk_start_stop.setText("Start/Stopp")
 
     def initComputerinformation(self):
+        self.pc_info = get_pc_information()
+        self.lb_x_value = 400
+        
+        self.current_user = self.pc_info["current_user"]
+        self.hostname = self.pc_info["hostname"]
+        self.ip = self.pc_info["ip_address"]
+        self.cpu_p = self.pc_info["cpu_p"]
+        self.cpu_l = self.pc_info["cpu_l"]
+        self.processor = self.pc_info["processor"]
+        self.os = self.pc_info["os"]
+        self.drives = self.pc_info["drives"]
+        self.memory = self.pc_info["memory"]
+
         self.tab_computerinformation = QWidget()
         self.tabWidget.addTab(self.tab_computerinformation, "Computerinformationen")
 
@@ -78,8 +94,8 @@ class Monitoring(QMainWindow):
         self.lb_user_description.setText("Angemeldeter Benutzer")
 
         self.lb_user_value = QLabel(self.tab_computerinformation)
-        self.lb_user_value.setGeometry(QRect(200, 15, 200, 25))
-        self.lb_user_value.setText("Hier steht der angemeldete User")
+        self.lb_user_value.setGeometry(QRect(200, 15, self.lb_x_value, 25))
+        self.lb_user_value.setText(self.current_user)
 
 
         self.lb_processes_description = QLabel(self.tab_computerinformation)
@@ -87,8 +103,8 @@ class Monitoring(QMainWindow):
         self.lb_processes_description.setText("Anzahl laufender Prozesse")
 
         self.lb_processes_value = QLabel(self.tab_computerinformation)
-        self.lb_processes_value.setGeometry(QRect(200, 40, 200, 25))
-        self.lb_processes_value.setText("Hier steht die Anzahl laufender Prozesse")
+        self.lb_processes_value.setGeometry(QRect(200, 40, self.lb_x_value, 25))
+        self.lb_processes_value.setText(str(len(psutil.pids())))
 
 
         self.lb_hostname_description = QLabel(self.tab_computerinformation)
@@ -96,8 +112,8 @@ class Monitoring(QMainWindow):
         self.lb_hostname_description.setText("Hostname")
 
         self.lb_hostname_value = QLabel(self.tab_computerinformation)
-        self.lb_hostname_value.setGeometry(QRect(200, 65, 200, 25))
-        self.lb_hostname_value.setText("Hier steht der Hostname")
+        self.lb_hostname_value.setGeometry(QRect(200, 65, self.lb_x_value, 25))
+        self.lb_hostname_value.setText(self.hostname)
 
 
         self.lb_user_description = QLabel(self.tab_computerinformation)
@@ -105,8 +121,8 @@ class Monitoring(QMainWindow):
         self.lb_user_description.setText("IP-Adresse")
 
         self.lb_user_value = QLabel(self.tab_computerinformation)
-        self.lb_user_value.setGeometry(QRect(200, 90, 200, 25))
-        self.lb_user_value.setText("Hier steht die IP-Adresse")
+        self.lb_user_value.setGeometry(QRect(200, 90, self.lb_x_value, 25))
+        self.lb_user_value.setText(self.ip)
 
 
         self.lb_count_physical_cores_description = QLabel(self.tab_computerinformation)
@@ -114,8 +130,8 @@ class Monitoring(QMainWindow):
         self.lb_count_physical_cores_description.setText("Anzahl physischer Kerne")
 
         self.lb_count_physical_cores_value = QLabel(self.tab_computerinformation)
-        self.lb_count_physical_cores_value.setGeometry(QRect(200, 115, 200, 25))
-        self.lb_count_physical_cores_value.setText("Hier steht die Anzahl physischer Kerne")
+        self.lb_count_physical_cores_value.setGeometry(QRect(200, 115, self.lb_x_value, 25))
+        self.lb_count_physical_cores_value.setText(str(self.cpu_p))
 
 
         self.lb_count_logical_cores_description = QLabel(self.tab_computerinformation)
@@ -123,8 +139,8 @@ class Monitoring(QMainWindow):
         self.lb_count_logical_cores_description.setText("Anzahl logischer Kerne")
 
         self.lb_count_logical_cores_value = QLabel(self.tab_computerinformation)
-        self.lb_count_logical_cores_value.setGeometry(QRect(200, 140, 200, 25))
-        self.lb_count_logical_cores_value.setText("Hier steht die Anzahl logischer Kerne")
+        self.lb_count_logical_cores_value.setGeometry(QRect(200, 140, self.lb_x_value, 25))
+        self.lb_count_logical_cores_value.setText(str(self.cpu_l))
         
 
         self.lb_processor_description = QLabel(self.tab_computerinformation)
@@ -132,8 +148,8 @@ class Monitoring(QMainWindow):
         self.lb_processor_description.setText("Verbauter Prozessor")
 
         self.lb_processor_value = QLabel(self.tab_computerinformation)
-        self.lb_processor_value.setGeometry(QRect(200, 165, 200, 25))
-        self.lb_processor_value.setText("Hier steht der verbaute Prozessor")
+        self.lb_processor_value.setGeometry(QRect(200, 165, self.lb_x_value, 25))
+        self.lb_processor_value.setText(self.processor)
 
 
         self.lb_os_description = QLabel(self.tab_computerinformation)
@@ -141,8 +157,8 @@ class Monitoring(QMainWindow):
         self.lb_os_description.setText("Betriebssystem")
 
         self.lb_os_value = QLabel(self.tab_computerinformation)
-        self.lb_os_value.setGeometry(QRect(200, 190, 200, 25))
-        self.lb_os_value.setText("Hier steht das Betriebssystem")
+        self.lb_os_value.setGeometry(QRect(200, 190, self.lb_x_value, 25))
+        self.lb_os_value.setText(self.os)
 
 
         self.lb_drives_description = QLabel(self.tab_computerinformation)
@@ -150,8 +166,8 @@ class Monitoring(QMainWindow):
         self.lb_drives_description.setText("Laufwerke")
 
         self.lb_drives_value = QLabel(self.tab_computerinformation)
-        self.lb_drives_value.setGeometry(QRect(200, 215, 200, 25))
-        self.lb_drives_value.setText("Hier sind alle Laufwerke gelistet")
+        self.lb_drives_value.setGeometry(QRect(200, 215, self.lb_x_value, 25))
+        self.lb_drives_value.setText(", ".join(self.drives))
 
 
         self.lb_ram_description = QLabel(self.tab_computerinformation)
@@ -159,8 +175,8 @@ class Monitoring(QMainWindow):
         self.lb_ram_description.setText("Verbauter Arbeitsspeicher")
 
         self.lb_ram_value = QLabel(self.tab_computerinformation)
-        self.lb_ram_value.setGeometry(QRect(200, 240, 200, 25))
-        self.lb_ram_value.setText("Verbauter Arbeitsspeicher")
+        self.lb_ram_value.setGeometry(QRect(200, 240, self.lb_x_value, 25))
+        self.lb_ram_value.setText(str(self.memory) + " GiB")
 
 
         self.btn_save_xml = QPushButton(self.tab_computerinformation)
@@ -186,6 +202,7 @@ class Monitoring(QMainWindow):
     def initGraph(self):
         self.tab_graph = QWidget()
         self.tabWidget.addTab(self.tab_graph, "Graph")
+
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
